e380b2657c72574631ddacb4846d8eb50926fd6a
Samuel <samnmbuguah@gmail.com>
2023-03-22 14:26:53 +0300

    Second Commit

--- Speed-Detector/index.js
+++ Speed-Detector/index.js
@@ -1,13 +1,15 @@
+//Initialize Variables from the HTML to be used by the functions
 const form = document.querySelector('form');
 const speedInput = document.querySelector('#speed');
 const demeritsTd = document.querySelector('#demerits');
 const licenseStatusTd = document.querySelector('#license-status');
 const clearButton = document.getElementById('clear-button');
 
-
+//Create an Event Listener for the submit button 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   
+  // Calculate the number of Demerits and add them to the table
   const speed = parseInt(speedInput.value);
   let demerits = 0;
   
@@ -16,14 +18,14 @@ form.addEventListener('submit', (e) => {
   }
   
   demeritsTd.textContent = `Points: ${demerits}`;
-  
+  //Determine whether the license should be suspended based on the number of demerits
   if (demerits > 12) {
     licenseStatusTd.textContent = 'License suspended';
   } else {
     licenseStatusTd.textContent = 'License active';
   }
 });
-
+//Clear the tables' cells
 clearButton.addEventListener('click', function() {
   demeritsTd.textContent = '';
   licenseStatusTd.textContent = '';
